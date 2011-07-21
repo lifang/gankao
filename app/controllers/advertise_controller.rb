d3bdc2c24114d94424cecea67e44b71c1e248a2f
lifang <santhe_2004@126.com>
2011-07-21 16:54:49 +0800

    update config --by lifang

--- app/controllers/advertise_controller.rb
+++ app/controllers/advertise_controller.rb
@@ -1,7 +1,8 @@
 class AdvertiseController < ApplicationController
 
   def index
-    @examination=Examination.find_by_sql("select * from examinations ex  where ex.exam_free_end_at>'#{Time.now.to_datetime}' order by exam_free_end_at asc limit 1")
+    @examination=Examination.find_by_sql("select * from examinations ex
+      where ex.exam_free_end_at>= '#{Time.now.to_date}' order by exam_free_end_at asc limit 1")
   end
 
   def lingqu
