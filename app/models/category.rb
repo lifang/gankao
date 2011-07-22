9de3a2e6693c48067e09ed601e2d26f6178c7918
lifang <santhe_2004@126.com>
2011-07-22 14:45:13 +0800

    lifang

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,8 @@ class Category < ActiveRecord::Base
   has_many :papers
   has_many :examinations
   #判断分类是否存在
+  FAURTH = 2
+  SIXTH = 3
   TYPES = {"2" => "english_fourth_level", "3" => "english_sixth_level"}   # :FOURTH_LEVEL 四级； :SIXTH_LEVEL 六级
   
   def Category.is_exists?(name)
