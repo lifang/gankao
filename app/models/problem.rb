dd5a6be9add44eac07e966197c4d28c0f7915be2
lifang <santhe_2004@126.com>
2011-09-21 09:32:33 +0800

    add problem type more blank --by lifang

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -10,8 +10,8 @@ class Problem < ActiveRecord::Base
   include REXML
   
   QUESTION_TYPE = {:SINGLE_CHOSE => 0, :MORE_CHOSE =>1, :JUDGE => 2, :SINGLE_CALK => 3,
-    :COLLIGATION => 4, :CHARACTER => 5 }
-  #0 单选题； 1 多选题；2 判断题；3 填空题； 4 综合题； 5 简答题
+    :COLLIGATION => 4, :CHARACTER => 5, :MORE_BLANKS => 6  }
+  #0 单选题； 1 多选题；2 判断题；3 填空题； 4 综合题； 5 简答题； 6 多选选择题
 
   #创建problem
   def Problem.create_problem(paper, options = {})
