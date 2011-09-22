cff4051b87f08fa219b536b69ecb86c48864c747
xuqiyong <jeffrey6052@163.com>
2011-09-22 15:33:34 +0800

    jeffrey

--- app/helpers/constant.rb
+++ app/helpers/constant.rb
@@ -28,7 +28,7 @@ module Constant
   BACK_SERVER_PATH = "http://localhost:3000"
   #项目文件目录
   PUBLIC_PATH = "#{Rails.root}/public"
-  BACK_PUBLIC_PATH = "f:/exam_app/public"
+  BACK_PUBLIC_PATH = "d:/exam_app/public"
   #试卷生成路径
   PAPER_PATH = BACK_SERVER_PATH+"/papers"
   #试卷服务器访问路径
@@ -70,7 +70,7 @@ module Constant
    VIP=10
 
   #非vip能做综合训练的次数
-  PRACTICES_COUNT = 5
+  PRACTICES_COUNT = 50
 
   
   
