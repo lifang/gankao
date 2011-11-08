fe48c4b288ccb38e7e61d6fe3c148ce7099a3a90
qianjun <er6788@126.com>
2011-11-08 11:43:16 +0800

    qianjun

--- public/javascripts/tab.js
+++ public/javascripts/tab.js
@@ -9,3 +9,30 @@ jQuery(function() {
 
     });
 })
+
+function tishi_hide(){
+    jQuery('.cz_tishi').hide();
+    jQuery('.zhezhao').hide();
+    return false;
+}
+
+
+function tishi(){
+    var doc_height = jQuery(document).height();
+    //var doc_width = $(document).width();
+    var win_height = jQuery(window).height();
+    var win_width = jQuery(window).width();
+    var s_layer_height = jQuery('.cz_tishi').height();
+    var s_layer_width = jQuery('.cz_tishi').width();
+    jQuery('.zhezhao').css('display','block');
+    jQuery('.zhezhao').css('height',doc_height);
+    jQuery('.cz_tishi').css('top',(win_height-s_layer_height)/2)
+    jQuery('.cz_tishi').css('left',(win_width-s_layer_width)/2);
+    jQuery('.cz_tishi').css('display','block');
+    //$('.serviceArea').fadeTo("slow", 1);
+    //alert($(document).height())
+    window.open("/competes/alipay_exercise")
+}
+
+
+
